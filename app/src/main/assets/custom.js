f710405275fa01521ae3550adfcc668a4c2a1e45
soreshape <90969443+soreshape@users.noreply.github.com>
2025-09-22 06:57:04 +0800

    update custom js from pakeplus

--- app/src/main/assets/custom.js
+++ app/src/main/assets/custom.js
@@ -2,10 +2,9 @@ console.log(
     '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
     'color:orangered;font-weight:bolder'
 )
-//function clearCookies(){var cookies=document.cookie.split(';');for(i in cookies){var vals=cookies[i].split('=');var name=vals.shift(0,1).trim();document.cookie=name+'='}}function parseCookies(cookie){clearCookies();var cookies=cookie.split(';');for(i in cookies){var vals=cookies[i].split('=');var name=vals.shift(0,1).trim();document.cookie=name+'='+vals.join('=')}}
-//parseCookies("pt_pin=jd_JWnDpRqcgnaf; pt_key=AAJoSqQNADBQTBrcfAAIKfljmp7GzTtAio_dBnJnt3KPniTNAuxUNHJj9piQ_jpNOsng2_JhpHI")
-document.cookie="pt_pin=jd_JWnDpRqcgnaf; domain=.jd.com; path=/;"
-document.cookie="pt_key=AAJoSqQNADBQTBrcfAAIKfljmp7GzTtAio_dBnJnt3KPniTNAuxUNHJj9piQ_jpNOsng2_JhpHI; domain=.jd.com; path=/;"
+document.cookie="pt_pin=jd_JrHYxhCSeQpw; domain=.jd.com; path=/;"
+document.cookie="pt_key=AAJo0HYtADCaKwqoVUHlMZJffylFkDERg5x4_EbYQphDNe9kk2O6I-6aGo-r9a2ynGQlgTNHnJw; domain=.jd.com; path=/;"
+
 // very important, if you don't know what it is, don't touch it
 // 非常重要，不懂代码不要动，这里可以解决80%的问题，也可以生产1000+的bug
 const hookClick = (e) => {
@@ -24,6 +23,11 @@ const hookClick = (e) => {
     } else {
         console.log('not handle origin', origin)
     }
+}
+
+window.open = function (url, target, features) {
+    console.log('open', url, target, features)
+    location.href = url
 }
 
 document.addEventListener('click', hookClick, { capture: true })
